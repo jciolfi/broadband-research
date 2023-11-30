74b7c09556b42f7fbe6d933332551c7697f9be92
John Ciolfi <johnc2744@gmail.com>
2023-11-29 22:54:06 -0500

    add analyses for rows 2-12

--- analysis.py
+++ analysis.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 
@@ -13,24 +14,25 @@ def extract_reports(start, stop):
         domain = row["Domain"]
         ip, msmt_id = row["IP"], row["Msmt_ID"]
         nbr_ip, nbr_msmt_id = row["Neighbor_IP"], row["Neighbor_Msmt_ID"]
-        target_report = f"report-{ip}_{domain}-{msmt_id}.csv"
-        nbr_report = f"report-{nbr_ip}-{nbr_msmt_id}"
-        reports.append((target_report, nbr_report))
+        target_report = f"./reports/report-{ip}_{domain}-{msmt_id}.csv"
+        nbr_report = f"./reports/report-{nbr_ip}-{nbr_msmt_id}.csv"
+        reports.append((i + 2, domain, target_report, nbr_report))
         
     return reports
 
 """
 For each IP
 - create path as string, e.g. ip (ASN) -> * -> ip ...
-- create a map of RTTs, e.g. (src, dst): [RTT1, RTT2]
+- create a map of RTTs, e.g. (src, dst): [RTT1, RTT2,...] and averages
 - map of ASN: times visited
 """
 def analyze(report_path):
     paths = []
-    rtts = {}
-    asns = {}
+    hop_rtts = {}
+    hop_count = {}
+    asn_count = {}
     
-    df = pd.read_csv(report_path, dtype={"ASN": str, "hop_ip": str})
+    df = pd.read_csv(report_path, dtype={"ASN": str, "hop_ip": str, "hop": str})
     cur_paths = [[], [], []]
     for index, row in df.iterrows():
         # break in trace - reset paths
@@ -43,28 +45,53 @@ def analyze(report_path):
         hop_ip = row["hop_ip"]
         rtt = row["RTT"]
         if isinstance(hop_ip, str):
-            if hop_ip in rtts:
-                rtts[hop_ip].append(rtt)
+            hop_count[hop_ip] = hop_count.get(hop_ip, 0) + 1
+            if hop_ip in hop_rtts:
+                hop_rtts[hop_ip].append(rtt)
             else:
-                rtts[hop_ip] = [rtt]
+                hop_rtts[hop_ip] = [rtt]
         else:
             hop_ip = "*"
-        cur_paths[int(row["pkt"]) - 1].append(hop_ip)
+        pkt_idx = int(row["pkt"]) - 1
+        if 0 <= pkt_idx < 3:
+            cur_paths[pkt_idx].append(hop_ip)
         
         asn = row["ASN"]
         if isinstance(asn, str):
-            asns[asn] = asns.get(asn, 0) + 1
+            asn_count[asn] = asn_count.get(asn, 0) + 1
         
     for cp in cur_paths:
         paths.append(" -> ".join(cp))
-    
-    print(f"{len(paths)}, {paths}\n\n{rtts}\n\n{asns}")
         
-            
+    avg_hop_rtt = {}
+    for hop, rtts in hop_rtts.items():
+        avg_hop_rtt[hop] = np.round(np.mean(rtts), decimals = 5)
+        
+    def format_paths(paths):
+        res = []
+        for path in paths:
+            res.append(f"        {path}")
+        return "\n\n".join(res)
     
+    return \
+    f"""        
+    avg_hop_rtt: {avg_hop_rtt}
+        
+    hop_count: {hop_count}
+        
+    asn_count: {asn_count}
     
-    
+    paths:
+{format_paths(paths)}
+    """
 
+
+def bulk_analyze(start_row, stop_row):
+    for row_num, domain, target_report, nbr_report in extract_reports(start_row, stop_row):
+        with open(f"./analyses/{row_num}-{domain}.txt", "w") as file:
+            file.write(f"{target_report}:\n{analyze(target_report)}")
+            file.write(f"{'-' * 200}\n\n")
+            file.write(f"{nbr_report}:\n\n{analyze(nbr_report)}")
 
 if __name__ == "__main__":
-    analyze("./reports/test.csv")
+    bulk_analyze(6, 6)
